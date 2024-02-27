8704997782c122effc0c8a4154181f8c9ad71ef5
Zihan Yang <ws704603301@gmail.com>
2024-02-27 16:56:33 -0500

    Add jump to the character, and it can jump only if touching the ground. Optimize the animation, only run if touching ground, keep jump if in the air

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,16 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 10f;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
 
     void Awake()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
     void Start()
     {
@@ -24,13 +27,13 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Run();
+        InAirAnimation();
         FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
     }
 
     void Run()
@@ -38,8 +41,25 @@ public class PlayerMovement : MonoBehaviour
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
         
+        // If the player is moving and touching the ground, then the player is running
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
-        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+        bool playerIsTouchingGround = myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed && playerIsTouchingGround);
+    }
+
+    void InAirAnimation()
+    {
+        myAnimator.SetBool("isJumping", !myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")));
+    }
+
+    void OnJump(InputValue value)
+    {
+        // If the player is not touching the ground, then the player cannot jump
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
     }
     
     void FlipSprite()
