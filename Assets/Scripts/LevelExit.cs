90a246caeb26b21e0b45e99cd0b96dc442e440ad
Zihan Yang <ws704603301@gmail.com>
2024-03-06 16:14:18 -0500

    Add Leve2, which is the same scene copied from Level 1, just for practicing the scene loading stuff

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 2f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextScene());
+
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(levelLoadDelay);
+        
+        // Get the current scene index (int)
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // Check if the current scene is the last scene in the build settings
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        // Load the next scene (which is a copy of the current scene)
+        SceneManager.LoadScene(nextSceneIndex);        
+    }
+}
