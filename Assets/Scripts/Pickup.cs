f5537a55c446e7b60e84fc1f2d35adae2c2f0203
Zihan Yang <ws704603301@gmail.com>
2024-03-07 13:29:48 -0500

    Add SFX for pickups, use PlayClipAtPoint() for learning

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     GameSession gameSession;
+    // AudioSource audioSource; // Doesn't work because the object is destroyed before the sound is played
+    [SerializeField] AudioClip pickupSFX;
     void Awake()
     {
         gameSession = FindObjectOfType<GameSession>();
+        // audioSource = GetComponent<AudioSource>(); // Doesn't work because the object is destroyed before the sound is played
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
+            // audioSource.Play(); // Doesn't work because the object is destroyed before the sound is played
+            AudioSource.PlayClipAtPoint(pickupSFX, Camera.main.transform.position); // This works
             Destroy(gameObject);
             gameSession.AddToScore(1);
         }
